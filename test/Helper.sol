ffc060840f6f5d5ba22fbd26c09210d8cda23631
Diego <105765223+0xfuturistic@users.noreply.github.com>
2023-08-11 15:39:55 +0200

    add events to Helper.sol

--- test/Helper.sol
+++ test/Helper.sol
@@ -4,31 +4,35 @@ pragma solidity ^0.8.13;
 import {Hooks} from "../src/Hooks.sol";
 
 contract HooksHelper is Hooks {
+    event SafePreHook(address target, string signature, bytes callData);
+
+    event SafePostHook(address target, string signature, bytes callData);
+
     function _preHooks(address target, string memory signature, bytes memory callData)
         internal
         preHook(target, signature, callData)
     {
-        // pass
+        // cannot emit event here bc it would modify the stack and make test fail
     }
 
     function _postHooks(address target, string memory signature, bytes memory callData)
         internal
         postHook(target, signature, callData)
     {
-        // pass
+        // cannot emit event here bc it would modify the stack and make test fail
     }
 
     function _safePreHooks(address target, string memory signature, bytes memory callData)
         internal
         safePreHook(target, signature, callData)
     {
-        // pass
+        emit SafePreHook(target, signature, callData);
     }
 
     function _safePostHooks(address target, string memory signature, bytes memory callData)
         internal
         safePostHook(target, signature, callData)
     {
-        // pass
+        emit SafePostHook(target, signature, callData);
     }
 }

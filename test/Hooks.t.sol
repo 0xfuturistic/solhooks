d6289a85a33ad35228e26ced2e0123e508ea4374
Diego <105765223+0xfuturistic@users.noreply.github.com>
2023-08-11 15:06:30 +0200

    Update Hooks.t.sol

--- test/Hooks.t.sol
+++ test/Hooks.t.sol
@@ -4,18 +4,24 @@ pragma solidity ^0.8.13;
 import {Test, console2} from "forge-std/Test.sol";
 import {Hooks} from "../src/Hooks.sol";
 
+import "./Helper.sol";
+
 contract HooksTest is Test, Hooks {
-    function setUp() public {}
+    HooksImplementer hooksImpl;
+
+    function setUp() public {
+        hooksImpl = new HooksImplementer();
+    }
 
     function test_preHooks(
         address target,
         string memory signature,
         bytes memory callData
     ) public {
-        if (!_staticcall(target, signature, callData)) {
+        if (!_call(target, signature, callData)) {
             vm.expectRevert();
         }
-        _preHooks(target, signature, callData);
+        hooksImpl.preHooks(target, signature, callData);
     }
 
     function test_postHooks(
@@ -23,31 +29,37 @@ contract HooksTest is Test, Hooks {
         string memory signature,
         bytes memory callData
     ) public {
+        if (!_call(target, signature, callData)) {
+            vm.expectRevert();
+        }
+        hooksImpl.postHooks(target, signature, callData);
+    }
+
+    function test_preStaticHooks(
+        address target,
+        string memory signature,
+        bytes memory callData
+    ) public {
         if (!_staticcall(target, signature, callData)) {
             vm.expectRevert();
         }
-        _postHooks(target, signature, callData);
+        hooksImpl.preStaticHooks(target, signature, callData);
+    }
+
+    function test_postStaticHooks(
+        address target,
+        string memory signature,
+        bytes memory callData
+    ) public {
+        if (!_staticcall(target, signature, callData)) {
+            vm.expectRevert();
+        }
+        hooksImpl.postStaticHooks(target, signature, callData);
     }
 
     /*//////////////////////////////////////////////////////////////
                                  INTERNAL
     //////////////////////////////////////////////////////////////*/
-
-    function _preHooks(
-        address target,
-        string memory signature,
-        bytes memory callData
-    ) internal preStaticHook(target, signature, callData) {
-        // pass
-    }
-
-    function _postHooks(
-        address target,
-        string memory signature,
-        bytes memory callData
-    ) internal preStaticHook(target, signature, callData) {
-        // pass
-    }
 
     function _call(
         address target,

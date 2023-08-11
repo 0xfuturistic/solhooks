2c0bd4cf38249a78ea7c2209ab9e085bdc3ea1f4
Diego <105765223+0xfuturistic@users.noreply.github.com>
2023-08-11 16:17:36 +0200

    Create Types.sol

--- src/Hooks.sol
+++ src/Hooks.sol
@@ -1,25 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-/// @dev Error for when a safe hook fails.
-/// @param source The address of the contract that initiated the hook.
-/// @param target The address of the contract that was the target of the hook.
-/// @param signature The signature of the function that was called as part of the hook.
-/// @param callData The calldata that was passed to the function as part of the hook.
-error SafeHookFailed(address source, address target, string signature, bytes callData);
+import "./Types.sol";
 
-/// @dev Error for when an unsafe hook fails.
-/// @param source The address of the contract that initiated the hook.
-/// @param target The address of the contract that was the target of the hook.
-/// @param signature The signature of the function that was called as part of the hook.
-/// @param callData The calldata that was passed to the function as part of the hook.
-error UnsafeHookFailed(address source, address target, string signature, bytes callData);
-
-/**
- * /// @title Hooks
- * /// @notice A contract that provides pre and post hooks for function calls to other contracts.
- */
-
+/// @title Hooks
+/// @notice A contract that provides pre and post hooks for function calls to other contracts.
 contract Hooks {
     /*//////////////////////////////////////////////////////////////
                                 MODIFIERS

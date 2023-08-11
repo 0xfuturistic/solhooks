e21675825ef3d803020adceb1ce2310b3a716724
Diego <105765223+0xfuturistic@users.noreply.github.com>
2023-08-11 02:15:51 +0200

    use fuzzer for tests

--- test/Hooks.t.sol
+++ test/Hooks.t.sol
@@ -7,28 +7,28 @@ import {Hooks} from "../src/Hooks.sol";
 contract HooksTest is Test, Hooks {
     function setUp() public {}
 
-    function test_preHooks() public {
-        preHooks(address(0), bytes4(0), abi.encode(0));
+    function test_preHooks(
+        address target,
+        bytes4 selector,
+        bytes memory callData
+    ) public {
+        preHooks(target, selector, callData);
     }
 
-    function testFail_preHooks() public {
-        preHooks(
-            address(this),
-            bytes4(keccak256("assertFail()")),
-            abi.encode(0)
-        );
+    function testFail_preHooks(bytes memory callData) public {
+        preHooks(address(this), bytes4(keccak256("assertFail()")), callData);
     }
 
-    function test_postHooks() public {
-        postHooks(address(0), bytes4(0), abi.encode(0));
+    function test_postHooks(
+        address target,
+        bytes4 selector,
+        bytes memory callData
+    ) public {
+        postHooks(target, selector, callData);
     }
 
-    function testFail_postHooks() public {
-        postHooks(
-            address(this),
-            bytes4(keccak256("assertFail()")),
-            abi.encode(0)
-        );
+    function testFail_postHooks(bytes memory callData) public {
+        postHooks(address(this), bytes4(keccak256("assertFail()")), callData);
     }
 
     function assertFail() public pure returns (bool) {

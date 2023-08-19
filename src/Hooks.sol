d46d1918fe7627f4ea333d400f43e3b5680d3186
Diego <0xfuturistic@pm.me>
2023-08-20 00:11:33 +0200

    add gas limit to hooks

--- src/Hooks.sol
+++ src/Hooks.sol
@@ -4,34 +4,46 @@ pragma solidity ^0.8.13;
 /// @title Hooks
 /// @notice A contract that provides pre and post hooks to arbitrary functions
 contract Hooks {
-    modifier preHook(function (bytes memory) external f, bytes memory input) {
+    error HookFailed();
+
+    modifier preHook(function (bytes memory) external f, bytes memory input, uint256 gasLimit) {
+        (bool success,) = f.address.call{gas: gasLimit}(abi.encodeWithSelector(f.selector, input));
+        if (!success) {
+            revert HookFailed();
+        }
+        _;
+    }
+
+    modifier postHook(function (bytes memory) external f, bytes memory input, uint256 gasLimit) {
+        _;
+        (bool success,) = f.address.call{gas: gasLimit}(abi.encodeWithSelector(f.selector, input));
+        if (!success) {
+            revert HookFailed();
+        }
+    }
+
+    modifier staticPreHook(function (bytes memory) external f, bytes memory input, uint256 gasLimit) {
+        (bool success,) = f.address.staticcall{gas: gasLimit}(abi.encodeWithSelector(f.selector, input));
+        if (!success) {
+            revert HookFailed();
+        }
+        _;
+    }
+
+    modifier staticPostHook(function (bytes memory) external f, bytes memory input, uint256 gasLimit) {
+        _;
+        (bool success,) = f.address.staticcall{gas: gasLimit}(abi.encodeWithSelector(f.selector, input));
+        if (!success) {
+            revert HookFailed();
+        }
+    }
+
+    modifier payablePreHook(function (bytes memory) payable external f, bytes memory input, uint256 gasLimit) {
         f(input);
         _;
     }
 
-    modifier postHook(function (bytes memory) external f, bytes memory input) {
-        _;
-        f(input);
-    }
-
-    modifier staticPreHook(function (bytes memory) external f, bytes memory input) {
-        (bool success,) = f.address.staticcall(abi.encodeWithSelector(f.selector, input));
-        require(success);
-        _;
-    }
-
-    modifier staticPostHook(function (bytes memory) external f, bytes memory input) {
-        _;
-        (bool success,) = f.address.staticcall(abi.encodeWithSelector(f.selector, input));
-        require(success);
-    }
-
-    modifier payablePreHook(function (bytes memory) payable external f, bytes memory input) {
-        f(input);
-        _;
-    }
-
-    modifier payablePostHook(function (bytes memory) payable external f, bytes memory input) {
+    modifier payablePostHook(function (bytes memory) payable external f, bytes memory input, uint256 gasLimit) {
         _;
         f(input);
     }

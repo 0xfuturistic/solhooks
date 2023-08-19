8937677d91d73c9c5746294d5dbaebff00d03991
Diego <0xfuturistic@pm.me>
2023-08-20 00:09:18 +0200

    replace calldata for memory

--- src/Hooks.sol
+++ src/Hooks.sol
@@ -4,34 +4,34 @@ pragma solidity ^0.8.13;
 /// @title Hooks
 /// @notice A contract that provides pre and post hooks to arbitrary functions
 contract Hooks {
-    modifier preHook(function (bytes calldata) external f, bytes calldata input) {
+    modifier preHook(function (bytes memory) external f, bytes memory input) {
         f(input);
         _;
     }
 
-    modifier postHook(function (bytes calldata) external f, bytes calldata input) {
+    modifier postHook(function (bytes memory) external f, bytes memory input) {
         _;
         f(input);
     }
 
-    modifier staticPreHook(function (bytes calldata) external f, bytes calldata input) {
+    modifier staticPreHook(function (bytes memory) external f, bytes memory input) {
         (bool success,) = f.address.staticcall(abi.encodeWithSelector(f.selector, input));
         require(success);
         _;
     }
 
-    modifier staticPostHook(function (bytes calldata) external f, bytes calldata input) {
+    modifier staticPostHook(function (bytes memory) external f, bytes memory input) {
         _;
         (bool success,) = f.address.staticcall(abi.encodeWithSelector(f.selector, input));
         require(success);
     }
 
-    modifier payablePreHook(function (bytes calldata) payable external f, bytes calldata input) {
+    modifier payablePreHook(function (bytes memory) payable external f, bytes memory input) {
         f(input);
         _;
     }
 
-    modifier payablePostHook(function (bytes calldata) payable external f, bytes calldata input) {
+    modifier payablePostHook(function (bytes memory) payable external f, bytes memory input) {
         _;
         f(input);
     }

1767776e58bdbdb90a77ec7395df650c2ec7008c
Diego <0xfuturistic@pm.me>
2023-09-01 12:30:02 -0700

    rewrite hooks

--- src/Hooks.sol
+++ src/Hooks.sol
@@ -2,49 +2,38 @@
 pragma solidity ^0.8.13;
 
 /// @title Hooks
-/// @notice A contract that provides pre and post hooks to arbitrary functions
 contract Hooks {
     error HookFailed();
 
-    modifier preHook(function (bytes memory) external f, bytes memory input, uint256 gasLimit) {
-        (bool success,) = f.address.call{gas: gasLimit}(abi.encodeWithSelector(f.selector, input));
+    modifier preHook(address funAddress, bytes4 funSelector, bytes memory input, uint256 gas) {
+        (bool success,) = funAddress.call{gas: gas}(abi.encodeWithSelector(funSelector, input));
         if (!success) {
             revert HookFailed();
         }
         _;
     }
 
-    modifier postHook(function (bytes memory) external f, bytes memory input, uint256 gasLimit) {
+    modifier postHook(address funAddress, bytes4 funSelector, bytes memory input, uint256 gas) {
         _;
-        (bool success,) = f.address.call{gas: gasLimit}(abi.encodeWithSelector(f.selector, input));
+        (bool success,) = funAddress.call{gas: gas}(abi.encodeWithSelector(funSelector, input));
         if (!success) {
             revert HookFailed();
         }
     }
 
-    modifier staticPreHook(function (bytes memory) external f, bytes memory input, uint256 gasLimit) {
-        (bool success,) = f.address.staticcall{gas: gasLimit}(abi.encodeWithSelector(f.selector, input));
+    modifier preHookStatic(address funAddress, bytes4 funSelector, bytes memory input, uint256 gas) {
+        (bool success,) = funAddress.staticcall{gas: gas}(abi.encodeWithSelector(funSelector, input));
         if (!success) {
             revert HookFailed();
         }
         _;
     }
 
-    modifier staticPostHook(function (bytes memory) external f, bytes memory input, uint256 gasLimit) {
+    modifier postHookStatic(address funAddress, bytes4 funSelector, bytes memory input, uint256 gas) {
         _;
-        (bool success,) = f.address.staticcall{gas: gasLimit}(abi.encodeWithSelector(f.selector, input));
+        (bool success,) = funAddress.staticcall{gas: gas}(abi.encodeWithSelector(funSelector, input));
         if (!success) {
             revert HookFailed();
         }
-    }
-
-    modifier payablePreHook(function (bytes memory) payable external f, bytes memory input, uint256 gasLimit) {
-        f(input);
-        _;
-    }
-
-    modifier payablePostHook(function (bytes memory) payable external f, bytes memory input, uint256 gasLimit) {
-        _;
-        f(input);
     }
 }
